767d44493cbbf87f16759cc5da5e1bd5f7a32735
Mohamed Jalloh <mohamedjalloh@Mohameds-MacBook-Air.local>
2019-02-13 12:23:51 -0500

    just created  a controller for songs

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'songs/index'
+  get 'songs/new'
+  get 'songs/create'
+  get 'songs/show'
+  get 'songs/edit'
+  get 'songs/update'
+  get 'songs/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
