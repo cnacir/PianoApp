367ebdf0919206c8e3033842b1fbf9ddc4903df1
Mohamed Jalloh <mohamedjalloh@Mohameds-MacBook-Air.local>
2019-02-13 12:52:07 -0500

    just added devise authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
-  get 'homes/index'
-  get 'songs/index'
-  get 'songs/new'
-  get 'songs/create'
-  get 'songs/show'
-  get 'songs/edit'
-  get 'songs/update'
-  get 'songs/destroy'
+  devise_for :users
+resources :songs
+resources :homes
+root 'homes#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
