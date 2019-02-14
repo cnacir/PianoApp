bb220cbc45d29a9c31284c341b3c963ad5c4ddc8
cnacir <cknacir@hotmail.com>
2019-02-14 12:32:28 -0500

    made profile page

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+	validates :username, uniqueness: true
+	validates :username, presence: true
 end
--- db/migrate/20190213175343_add_referencein_songs_model.rb
+++
@@ -1,5 +0,0 @@
-class AddReferenceinSongsModel < ActiveRecord::Migration[5.2]
-  def change
-    add_reference :songs, :user, index: true 
-  end
-end
