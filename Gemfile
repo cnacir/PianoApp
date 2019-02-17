c6c54a19c96c887d24957e59034eb7770526187f
cnacir <cknacir@hotmail.com>
2019-02-16 21:31:12 -0500

    built the piano

--- Gemfile
+++ Gemfile
@@ -4,6 +4,8 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 ruby '2.6.0'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
+gem 'bootstrap'
+gem 'jquery-rails'
 gem 'rails', '~> 5.2.2'
 # Use postgresql as the database for Active Record
 gem 'pg', '>= 0.18', '< 2.0'
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,46 +10,11 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery3
+//= require popper
+//= require bootstrap
+//= require bootstrap-sprockets
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
 //= require_tree .
-
-
-// Create the audio context
-
-var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-
-// Initialize Sound class
-
-class Sound {
-
-  constructor(audioCtx) {
-    this.audioCtx = audioCtx;
-  }
-
-  setup() {
-    this.oscillator = this.audioCtx.createOscillator();
-    this.gainNode = this.audioCtx.createGain();
-
-    this.oscillator.connect(this.gainNode);
-    this.gainNode.connect(this.audioCtx.destination);
-    this.oscillator.type = 'sine';
-  }
-
-  play(value) {
-    this.setup();
-    this.oscillator.frequency.value = value;
-    this.gainNode.gain.setValueAtTime(0, this.context.currentTime);
-    this.gainNode.gain.linearRampToValueAtTime(1, this.audioCtx.currentTime + 0.01);
-    this.oscillator.start(audioCtx.currentTime);
-    this.stop(this.audioCtx.currentTime);
-
-  }
-
-  stop() {
-    this.gainNode.gain.exponentialRampToValueAtTime(0.001, this.audioCtx.currentTime + 1);
-    this.oscillator.stop(this.audioCtx.currentTime + 1);
-  }
-
-}
